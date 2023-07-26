c94846faf5af67c903f9dbaa8488fd3b871f0f64
Erik Garrison <erik.garrison@gmail.com>
2023-07-26 11:38:16 +0200

    matches R prcomp

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,72 @@
-use ndarray::{Array1, Array2, Axis};
-use ndarray_linalg::SVD;
-use ndarray::s;
+use ndarray::{Array1, Array2, Axis, s};
+use ndarray_linalg::svd::SVD;
 use std::error::Error;
 
 pub struct PCA {
-    components: Option<Array2<f64>>,
+    rotation: Option<Array2<f64>>,
     mean: Option<Array1<f64>>,
-    scale: bool,
+    scale: Option<Array1<f64>>,
+}
+
+impl Default for PCA {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl PCA {
-    pub fn new(scale: bool) -> Self {
+    pub fn new() -> Self {
         Self {
-            components: None,
+            rotation: None,
             mean: None,
-            scale,
+            scale: None,
         }
     }
 
-    pub fn fit(&mut self, mut x: Array2<f64>, n_components: usize) -> Result<(), Box<dyn Error>> {
-        let n_samples = x.nrows();
-        if n_samples < 2 {
+    pub fn fit(&mut self, mut x: Array2<f64>, tol: Option<f64>) -> Result<(), Box<dyn Error>> {
+        let n = x.nrows();
+        if n < 2 {
             return Err("Input matrix must have at least 2 rows.".into());
         }
 
-        let mean = x.mean_axis(Axis(0)).unwrap();
+        // Compute mean for centering
+        let mean = x.mean_axis(Axis(0)).ok_or("Failed to compute mean")?;
         self.mean = Some(mean.clone());
         x -= &mean;
 
-        if self.scale {
-            let std_dev = x.map_axis(Axis(0), |v| v.std(1.0));
-            x /= &std_dev.mapv(|v| if v != 0. { v } else { 1. });
+        // Compute scale
+        let std_dev = x.map_axis(Axis(0), |v| v.std(1.0));
+        self.scale = Some(std_dev.clone());
+        x /= &std_dev.mapv(|v| if v != 0. { v } else { 1. });
+
+        let k = std::cmp::min(n, x.ncols());
+
+        // Compute SVD
+        let (_u, mut s, vt) = x.svd(true, true).map_err(|_| "Failed to compute SVD")?;
+
+        // Normalize singular values
+        s.mapv_inplace(|v| v / ((n as f64 - 1.0).max(1.0)).sqrt());
+
+        // Compute Rotation
+        let rotation;
+        if let Some(t) = tol {
+            let threshold = s[0] * t;
+            let rank = s.iter().take_while(|&si| *si > threshold).count();
+            rotation = vt.unwrap().slice_move(s![..std::cmp::min(rank, k), ..]).reversed_axes();
+        } else {
+            rotation = vt.unwrap().slice_move(s![..k, ..]).reversed_axes();
         }
+        self.rotation = Some(rotation);
 
-        let (_, _s, v) = x.svd(true, true).unwrap();
-        let mut v = v.unwrap();
-        v = v.slice(s![.., 0..n_components]).to_owned();
-
-        self.components = Some(v);
         Ok(())
     }
 
     pub fn transform(&self, mut x: Array2<f64>) -> Result<Array2<f64>, Box<dyn Error>> {
-        match (&self.components, &self.mean) {
-            (Some(components), Some(mean)) => {
+        match (&self.rotation, &self.mean, &self.scale) {
+            (Some(rotation), Some(mean), Some(scale)) => {
                 x -= mean;
-                if self.scale {
-                    let std_dev = x.map_axis(Axis(0), |v| v.std(1.0));
-                    x /= &std_dev.mapv(|v| if v != 0. { v } else { 1. });
-                }
-                Ok(x.dot(components))
+                x /= scale;
+                Ok(x.dot(rotation))
             }
             _ => Err("PCA not fitted yet.".into()),
         }
@@ -63,9 +80,9 @@ mod tests {
     use super::*;
     use float_cmp::approx_eq;
 
-    fn test_pca(input: Array2<f64>, expected_output: Array2<f64>, n_components: usize) {
-        let mut pca = PCA::new(true);
-        pca.fit(input.clone(), n_components).unwrap();
+    fn test_pca(input: Array2<f64>, expected_output: Array2<f64>, tol: Option<f64>) {
+        let mut pca = PCA::new();
+        pca.fit(input.clone(), tol).unwrap();
         let output = pca.transform(input).unwrap();
 
         eprintln!("output: {:?}", output);
@@ -76,11 +93,9 @@ mod tests {
         let expected_output_abs = expected_output.mapv_into(f64::abs);
 
         // Compare arrays
-        //eprintln!("output: {:?}", output_abs);
-        //eprintln!("expected_output: {:?}", expected_output_abs);
         let equal = output_abs.shape() == expected_output_abs.shape() &&
             output_abs.iter().zip(expected_output_abs.iter())
-                      .all(|(a, b)| approx_eq!(f64, *a, *b, epsilon = 1.0e-8));
+                      .all(|(a, b)| approx_eq!(f64, *a, *b, epsilon = 1.0e-6));
         assert!(equal);
     }
 
@@ -91,7 +106,7 @@ mod tests {
         let expected = array![[-1.0, 4.109492e-16],
                               [1.0, 2.647088e-16]];
 
-        test_pca(input, expected, 2);
+        test_pca(input, expected, None);
     }
     
     #[test]
@@ -104,10 +119,9 @@ mod tests {
                               [-1.098771, -1.03624808, 1.566910e-16],
                               [2.295834, 0.02916061, 3.427952e-17]];
 
-        test_pca(input, expected, 3);
+        test_pca(input, expected, None);
     }
 
-    /*
     #[test]  
     fn test_pca_5x5() {
         let input = array![[0.5855288, -1.8179560, -0.1162478, 0.8168998, 0.7796219],
@@ -122,7 +136,7 @@ mod tests {
                               [1.0944225, -1.5788366, -0.57193457, -0.2771233, 4.276631e-16],
                               [1.1362824, 0.6010291, 1.07545409, -0.2597919, -6.793015e-17]];
 
-        test_pca(input, expected, 5);
+        test_pca(input, expected, None);
     }
 
     #[test]
@@ -139,17 +153,16 @@ mod tests {
                               [0.5487077, 1.8825193, -0.30961285, 0.511856077, 3.404246e-15],
                               [1.3564746, -0.5038210, 1.77985239, -0.003410674, -3.187086e-16]];
 
-        test_pca(input, expected, 5);
-}
-    */
+        test_pca(input, expected, None);
+    }
 
     #[test]
     #[should_panic]
     fn test_pca_fit_insufficient_samples() {
         let x = array![[1.0]];
         
-        let mut pca = PCA::new(true);
-        pca.fit(x, 1).unwrap(); 
+        let mut pca = PCA::new();
+        pca.fit(x, None).unwrap();
     }
 
     #[test]
@@ -157,7 +170,7 @@ mod tests {
     fn test_pca_transform_not_fitted() {
         let x = array![[1.0, 2.0]];
         
-        let pca = PCA::new(true);
+        let pca = PCA::new();
         let _ = pca.transform(x).unwrap();
     }
 }
